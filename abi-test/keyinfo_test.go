07895b33a833e0672003d499579ecac5bb1a2ab0
Marius van der Wijden <m.vanderwijden@live.de>
2020-10-27 11:39:50 +0100

    abi-test: added keyinfo

---
+++ abi-test/keyinfo_test.go
@@ -0,0 +1,50 @@
+package geth
+
+import (
+	"crypto/ecdsa"
+	"math/big"
+	"testing"
+
+	"github.com/ethereum/go-ethereum/accounts/abi/bind"
+	"github.com/ethereum/go-ethereum/accounts/abi/bind/backends"
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/core"
+	"github.com/ethereum/go-ethereum/crypto"
+)
+
+func TestKeyInfo(t *testing.T) {
+	backend, sk := getSimBackend()
+	transactor := bind.NewKeyedTransactor(sk)
+	_, _, tx, err := DeployKeyInfo(transactor, backend)
+	if err != nil {
+		t.Fail()
+	}
+	backend.Commit()
+	_ = tx
+}
+
+var (
+	SK   = "0xcdfbe6f7602f67a97602e3e9fc24cde1cdffa88acd47745c0b84c5ff55891e1b"
+	ADDR = "0xb02A2EdA1b317FBd16760128836B0Ac59B560e9D"
+)
+
+func getSimBackend() (*backends.SimulatedBackend, *ecdsa.PrivateKey) {
+	sk, err := crypto.GenerateKey()
+	if err != nil {
+		panic(err)
+	}
+	faucetAddr := crypto.PubkeyToAddress(sk.PublicKey)
+	addr := map[common.Address]core.GenesisAccount{
+		common.BytesToAddress([]byte{1}): {Balance: big.NewInt(1)}, // ECRecover
+		common.BytesToAddress([]byte{2}): {Balance: big.NewInt(1)}, // SHA256
+		common.BytesToAddress([]byte{3}): {Balance: big.NewInt(1)}, // RIPEMD
+		common.BytesToAddress([]byte{4}): {Balance: big.NewInt(1)}, // Identity
+		common.BytesToAddress([]byte{5}): {Balance: big.NewInt(1)}, // ModExp
+		common.BytesToAddress([]byte{6}): {Balance: big.NewInt(1)}, // ECAdd
+		common.BytesToAddress([]byte{7}): {Balance: big.NewInt(1)}, // ECScalarMul
+		common.BytesToAddress([]byte{8}): {Balance: big.NewInt(1)}, // ECPairing
+		faucetAddr:                       {Balance: new(big.Int).Sub(new(big.Int).Lsh(big.NewInt(1), 256), big.NewInt(9))},
+	}
+	alloc := core.GenesisAlloc(addr)
+	return backends.NewSimulatedBackend(alloc, 80000000), sk
+}
